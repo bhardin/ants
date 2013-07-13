90f4dfcb6980a7a92328da499bd464da44e30aeb
Brett Hardin <bhardin@kissmetrics.com>
2013-07-13 10:22:12 -0700

    Fixing match submission

--- app/models/match.rb
+++ app/models/match.rb
@@ -9,12 +9,11 @@ class Match < ActiveRecord::Base
   attr_accessible :game_1_score, :game_2_score
 
   def finished?
-    self.status == :finished
+    self.status == "finished"
   end
 
   def update_score(match, params)
     if params["game_1_score"]
-      puts "here"
       temp_score = params["game_1_score"].tr(' ', '').split("-")
       self.player_1_game_1_score = temp_score[0]
       self.player_2_game_1_score = temp_score[1]
@@ -25,6 +24,8 @@ class Match < ActiveRecord::Base
       self.player_2_game_2_score = temp_score[1]
       self.status = "Game 2 Finished"
     end
+
+    self.save
   end
 
   def player_1
